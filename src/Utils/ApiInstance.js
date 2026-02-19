fbb4f1ce7008a9d99e692d98a7f3a6d4718afc0d
akshayauchar07 <akshayauchar1998@gmail.com>
2026-02-19 16:39:50 +0530

    fix all casing

--- src/Utils/ApiInstance.js
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-
-export const ApiInstance = axios.create({
-  baseURL: "https://hiring-assignment.wobot.ai/api/v1",
-  headers: {
-    Authorization: `Bearer 4ApVMIn5sTxeW7GQ5VWeWiy`,
-  },
-});
